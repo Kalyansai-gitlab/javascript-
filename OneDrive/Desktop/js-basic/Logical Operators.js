bf3e20ae6cf1010a59414a76698fb811d350e7df
Kalyansai-gitlab <114100430+Kalyansai-gitlab@users.noreply.github.com>
2022-12-28 17:34:02 +0530

    Add files via upload

---
+++ OneDrive/Desktop/js-basic/Logical Operators.js
@@ -0,0 +1,26 @@
+console.log("Hello World");
+let x = 7, y = 8, z = 9
+
+let result = x < y && x < z
+console.log(result);
+
+let x1 = 7, y1 = 8, z1 = 5
+
+let result1 = x1 < y1 && x1 < z1
+console.log(result1);
+
+//let x2 = 7, y2 = 2, z2 = 5
+let x2 = 7, y2 = 8, z2 = 5
+
+
+let result2 = x2 < y2 || x2 < z2
+console.log(result2);
+
+
+//let x3 = 7, y3 = 8, z3 = 9
+let x3 = 7, y3 = 2, z3 = 5
+
+let result3 = x3 < y3 && x3 < z3
+let n = !result3;
+console.log(result3 , n);
+
