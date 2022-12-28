03be9deafe036b8d5cc3ef1ab8b4283a17238d82
Kalyansai-gitlab <114100430+Kalyansai-gitlab@users.noreply.github.com>
2022-12-28 17:32:11 +0530

    Add files via upload

---
+++ OneDrive/Desktop/js-basic/Relational operators.js
@@ -0,0 +1,46 @@
+console.log("Hello World");
+
+console.log(8 > 9);
+
+console.log(8 < 9);
+ 
+let date = 7 > 9
+console.log(date);
+
+let date1 = 7 <= 9
+console.log(date1);
+
+let date2 = 9 >= 8
+console.log(date2);
+
+let date3 = 7 >= 7
+console.log(date3);
+
+let date4 = 6 <= 6
+console.log(date4);
+console.log(typeof date3,date2,date4);
+
+let x = 12
+let y = 14
+let  date5 = x > y
+console.log(date5);
+
+let a = 35
+let b = 55
+let z = a + b;
+console.log(z);
+
+let c = 10
+let d = 3.57
+let w = c - d;
+console.log(w);
+
+let x1 = "3"
+let y1 =  3
+let date6 = x1 ===  y1;
+console.log(date5);
+
+let x2 =  3
+let y2 =  3
+let date7 = x2 ===  y2;
+console.log(date7);
