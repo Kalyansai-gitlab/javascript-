acbd9eae44e8077c2f94fd9b7da80866ecf3b6e3
Kalyansai-gitlab <114100430+Kalyansai-gitlab@users.noreply.github.com>
2022-12-28 15:01:08 +0530

    Add files via upload

---
+++ OneDrive/Desktop/js-basic/data types.js
@@ -0,0 +1,35 @@
+console.log("Hello, World!");
+let data = 8
+let user = "kalyan"
+
+let num1 =0xfff
+console.log(num1);
+console.log(typeof user);
+console.log(typeof data);
+
+let num2 = -5/0
+console.log(num2);
+
+let num3 = 100_00_000
+console.log(num3);
+
+let bool1 = 6 > 8 
+console.log(bool1);
+
+let bool = 5 < 6
+console.log(typeof bool);
+
+let user1
+console.log(typeof user1);
+
+let firstname = "kalyan"
+let lastname = "sai" 
+let user2 = firstname +" "+ lastname
+console.log(user2);
+
+let user3 = 'kalyan"sai"'
+console.log(user3);
+
+let user4 = "kalyan\"sai\""
+console.log(user4);
+
