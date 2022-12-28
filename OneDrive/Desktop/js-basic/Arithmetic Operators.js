da2cce82ed3ceb3f826de52102fe62d51067d111
Pushpa <pushpa@triwish.io>
2022-12-28 14:30:41 +0530

    firstcommit

---
+++ OneDrive/Desktop/js-basic/Arithmetic Operators.js
@@ -0,0 +1,21 @@
+console.log("Hello, World!");
+let num1 = 4
+let num2 = 2
+let result = num1 + num2
+console.log( result );
+
+let num3 = 15.5
+let num4 = 10
+let result1 = num3 / num4
+console.log( result1 );
+
+let num5 = 26
+let num6 = 9
+let result2 = num5 * num6
+console.log( result2 );
+
+let num7 = 39
+let num8 = 20
+let result3 = num7 % num8
+console.log( result3 );
+
