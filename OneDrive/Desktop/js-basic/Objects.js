95ff438e961ced987fb4190bb3846e9488514ff4
Kalyansai-gitlab <114100430+Kalyansai-gitlab@users.noreply.github.com>
2023-01-04 17:11:24 +0530

    Add files via upload

---
+++ OneDrive/Desktop/js-basic/Objects.js
@@ -0,0 +1,19 @@
+let age = 26
+let alien ={
+    name: 'kalyan',
+    tech: 'js',
+    age:   26,
+    laptop:{
+        cpu:'i10',
+        ram: 8 ,
+        brand: 'hp'
+    }
+    
+}
+
+console.log(alien.name);
+console.log(alien.tech);
+console.log(alien['age']);
+console.log(alien.laptop.brand);
+//"break";
+console.log(alien);
